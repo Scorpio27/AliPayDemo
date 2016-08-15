35d0067dc58bd082cb1f52bee80d8131ed7abe8c
Apple <apple@appledeimac.lan>
2016-08-15 15:08:18 +0800

    Initial Commit

---
+++ AliPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AliPayDemo
+//
+//  Created by Apple on 16/8/15.
+//  Copyright © 2016年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AliPayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AliPayDemo
+//
+//  Created by Apple on 16/8/15.
+//  Copyright © 2016年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
